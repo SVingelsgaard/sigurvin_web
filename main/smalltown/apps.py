6af5f09e62eef950b5f59c216be7fb74d7f0b341
SVingelsgaard <sigurd@vingelsgaard.no>
2022-06-05 20:48:54 +0200

    x

---
+++ main/smalltown/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SmalltownConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'smalltown'
---
+++ main/smalltown/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from . import views
+
+appName = 'smalltown'
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
