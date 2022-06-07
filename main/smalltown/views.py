c8616aaf151bb895a04343690ecd411de88017cb
SVingelsgaard <sigurd@vingelsgaard.no>
2022-06-07 22:43:14 +0200

    x

--- main/smalltown/views.py
+++ main/smalltown/views.py
@@ -6,5 +6,6 @@ def home(request):
     if request.method == "POST":
         grocery = request.POST.get("grocery")
         Shoppinglist.objects.create(grocery = grocery, description = "blanch")
+        #shoud make it so i the item is not named shoppinglist
 
     return render(request, "smalltown.html", {"Shoppinglist": Shoppinglist.objects.all})
