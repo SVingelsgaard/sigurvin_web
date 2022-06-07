a8596c47df4deae66157d9ba49282a719680081c
SVingelsgaard <sigurd@vingelsgaard.no>
2022-06-07 22:42:37 +0200

    working ish

--- main/smalltown/forms.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import fields
-from django.forms import ModelForm
-
-from .models import *
-
-class ShoppinglistForm(ModelForm):
-    class Meta:
-        model = Shoppinglist
-        fields = ["grocery", "description"]
--- main/smalltown/views.py
+++ main/smalltown/views.py
@@ -1,13 +1,10 @@
 from django.shortcuts import render
-from .models import *
-from .forms import *
+from .models import Shoppinglist
 
-# Create your views here.
 #hompage
 def home(request):
     if request.method == "POST":
         grocery = request.POST.get("grocery")
         Shoppinglist.objects.create(grocery = grocery, description = "blanch")
-        
-    myShoppinglist = Shoppinglist.objects.all
-    return render(request, "smalltown.html", {"Shoppinglist": myShoppinglist})
+
+    return render(request, "smalltown.html", {"Shoppinglist": Shoppinglist.objects.all})
