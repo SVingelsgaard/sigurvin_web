7ed52477964f29c87a81c7bb2119e3a65c02d9a6
SVingelsgaard <sigurd@vingelsgaard.no>
2022-06-08 19:46:56 +0200

    reee

--- main/smalltown/urls.py
+++ main/smalltown/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from . import views
 
-appName = 'smalltown'
+appName = "smalltown"
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path("", views.home, name="home"),
+    path("add_grocery", views.add_grocery, name="add_grocery"),
+    path("remove_grocery", views.remove_grocery, name="remove_grocery"),
 ]
--- main/smalltown/views.py
+++ main/smalltown/views.py
@@ -1,11 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Shoppinglist
 
 #hompage
 def home(request):
+    return render(request, "smalltown.html", {"Shoppinglist": Shoppinglist.objects.all})
+
+def add_grocery(request):
     if request.method == "POST":
         grocery = request.POST.get("grocery")
         Shoppinglist.objects.create(grocery = grocery, description = "blanch")
         #shoud make it so i the item is not named shoppinglist
+        
+    return redirect("home")
 
-    return render(request, "smalltown.html", {"Shoppinglist": Shoppinglist.objects.all})
+def remove_grocery(request):
+    if request.method == "POST":
+        id = request.POST.get("Shopping")
+        print(request.POST.get(Shoppinglist))
+    return redirect("home")
